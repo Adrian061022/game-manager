2da526579129f9f01ed5f38e5146cc8955eb07a5
Adrian061022 <markusadrian2006@gmail.com>
2026-03-20 13:31:38 +0100

    GameFactory megírva

--- database/factories/GameFactory.php
+++ database/factories/GameFactory.php
@@ -17,7 +17,11 @@ class GameFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->unique()->sentence(3),
+            'description' => fake()->paragraph(5),
+            'price' => fake()->randomFloat(2, 4.99, 59.99),
+            'cover_image' => fake()->imageUrl(460, 215, 'games', true),
+            'category_id' => \App\Models\Category::factory(),
         ];
     }
 }
