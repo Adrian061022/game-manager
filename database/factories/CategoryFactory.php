24b34a3234fed4b209ca98f6d52bbc0af7cbb193
Adrian061022 <markusadrian2006@gmail.com>
2026-03-20 13:31:09 +0100

    CategoryFactory megírva

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -16,8 +16,10 @@ class CategoryFactory extends Factory
      */
     public function definition(): array
     {
+        $name = fake()->unique()->words(2, true);
         return [
-            //
+            'name' => ucfirst($name),
+            'slug' => \Illuminate\Support\Str::slug($name),
         ];
     }
 }
